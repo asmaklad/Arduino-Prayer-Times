e2f694516dbda97318fc24d97c3188aae15528c0
asmaklad <asmaklad@hotmail.com>
2015-03-05 21:34:37 +0100

    new file:   PrayerTimes.h 	new file:   PrayerTimes_example.ino 	new file:   Readme.txt.txt

---
+++ PrayerTimes_example.ino
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <Time.h>
+#include <Wire.h>
+#include "PrayerTimes.h"
+
+double times[sizeof(TimeName)/sizeof(char*)];
+
+void p(char *fmt, ... ){
+        char tmp[128]; // resulting string limited to 128 chars
+        va_list args;
+        va_start (args, fmt );
+        vsnprintf(tmp, 128, fmt, args);
+        va_end (args);
+        Serial.print(tmp);
+}
+
+
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+
+  Serial.println("PTimes:");
+  //get_prayer_times(year(), month(), day(), latitude, longitude, timezone, &times);
+  set_calc_method(ISNA);
+  set_asr_method(Shafii);
+  //set_high_lats_adjust_method(AngleBased);
+  //set_fajr_angle(15);
+  //set_maghrib_angle(15);
+  //set_isha_angle(15);
+  int dst=1;
+  float latitude=46.9500;
+  float longitude=7.4458;
+  get_prayer_times(year(), month(), day(), latitude, longitude, dst, times);
+  for (int i=0;i<sizeof(times)/sizeof(double);i++){
+    char tmp[10];
+    int hours, minutes;
+    get_float_time_parts(times[i], hours, minutes);
+    p("%d \t %10s %s \t %02d:%02d \n\r",i,TimeName[i],dtostrf(times[i],2,2,tmp),hours,minutes);
+    /*
+    Serial.print(i); 
+    Serial.print(" \t "); 
+    Serial.print(TimeName[i]); 
+    Serial.print(" \t\t "); 
+    Serial.print(times[i]);
+    Serial.print(" \t ");     
+    Serial.print(hours); 
+    Serial.print(":"); 
+    Serial.print(minutes); 
+    Serial.println();
+    */
+  }
+  delay(10000);
+}
---
+++ Readme.txt.txt
@@ -0,0 +1,6 @@
+Islamic Prayer Times for Arduino.
+
+so far worked only on Arduino Mega 2560 
+with Uno I get arbitary results.
+
+Adabted for arduino from this site: http://praytimes.org/manual/
